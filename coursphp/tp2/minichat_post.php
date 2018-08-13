d7e767566612c2c1fddba19cf04b4ebb3b74c852
floflimctb <floflo.brotons@gmail.com>
2018-08-13 15:29:25 +0200

    Update tp2

--- coursphp/tp2/minichat_post.php
+++ coursphp/tp2/minichat_post.php
@@ -4,7 +4,7 @@
 // Puis rediriger vers minichat.php comme ceci :
 
 try { 
-	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
 } catch (Exception $e) { 
 	die('Erreur : ' . $e->getMessage()); 
 }
