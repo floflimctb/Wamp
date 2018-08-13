cfc2ab4b100007eba48e503bfb39aa3c81dafce6
floflimctb <floflo.brotons@gmail.com>
2018-08-13 18:51:39 +0200

    Update tp

--- coursphp/tp2/Correction/minichat.php
+++ coursphp/tp2/Correction/minichat.php
@@ -1,3 +1,9 @@
+<?php
+
+//setcookie('pseudo', '', time() + 365*24*3600, null, null, false, true);
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,44 +11,44 @@
         <title>Mini-chat</title>
     </head>
     <style>
-    form
-    {
+    form {
         text-align:center;
     }
     </style>
+
     <body>
     
-    <form action="minichat_post.php" method="post">
-        <p>
-        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
-        <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
+        <form action="minichat_post.php" method="post">
+            <p>
+            <label for="pseudo">Pseudo:</label>
+            <input type="text" name="pseudo" id="pseudo"  value=<?php $_COOKIE['pseudo'] ?> />
+            </p>
+            <p>
+            <label for="message">Message:</label>
+            <input type="text" name="message" id="message" />
+            </p>
+            <p>
+            <input type="submit" value="Envoyer">
+            </p>
+        </form>
 
-        <input type="submit" value="Envoyer" />
-	</p>
-    </form>
+        <?php
 
-<?php
-// Connexion à la base de données
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
+            try {
+            	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+            }
+            catch (Exception $e) {
+                    die('Erreur : '.$e->getMessage());
+            }
 
-// Récupération des 10 derniers messages
-$reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 0, 10');
+            $reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 0, 10');
 
-// Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
-while ($donnees = $reponse->fetch())
-{
-	echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
-}
+            while ($donnees = $reponse->fetch()) {
+            	echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+            }
 
-$reponse->closeCursor();
+            $reponse->closeCursor();
 
-?>
+        ?>
     </body>
 </html>
--- coursphp/tp2/Correction/minichat_post.php
+++ coursphp/tp2/Correction/minichat_post.php
@@ -1,18 +1,17 @@
 <?php
 // Connexion à la base de données
-try
-{
+
+setcookie('pseudo', $_POST['pseudo'], time() + 365*24*3600, null, null, false, true);
+
+try {
 	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
 }
-catch(Exception $e)
-{
+catch (Exception $e) {
         die('Erreur : '.$e->getMessage());
 }
 
-// Insertion du message à l'aide d'une requête préparée
 $req = $bdd->prepare('INSERT INTO minichat (pseudo, message) VALUES(?, ?)');
 $req->execute(array($_POST['pseudo'], $_POST['message']));
 
-// Redirection du visiteur vers la page du minichat
 header('Location: minichat.php');
 ?>
